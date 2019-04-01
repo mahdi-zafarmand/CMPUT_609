7b0f06213eff5938d08a897ebe3f0ea4ed5b224d
mahdi-zafarmand <38989642+mahdi-zafarmand@users.noreply.github.com>
2019-03-31 20:17:20 -0600

    Update reinforcement_learning_project_code.py

--- reinforcement_learning_project_code.py
+++ reinforcement_learning_project_code.py
@@ -12,8 +12,8 @@ class World:
         self.actions = {}
         self.rewards = {}
         self.initialize()
-        self.policy = np.zeros((len(self.states), len(self.actions['actions'])))
-        self.Q = np.zeros(self.policy.shape)
+        self.P = np.zeros((len(self.states), len(self.actions['actions'])))
+        self.Q = np.zeros(self.P.shape)
 
     def initialize(self):
         self.create_environment_1()
@@ -87,14 +87,14 @@ class World:
 
     def make_greedy(self, s, epsilon):
         ACTIONS = self.actions['actions']
-        self.policy[s, :] = [epsilon / (len(ACTIONS) - 1.)] * len(ACTIONS)
+        self.P[s, :] = [epsilon / (len(ACTIONS) - 1.)] * len(ACTIONS)
         best_a = np.argmax(self.Q[s, :])
-        self.policy[s, best_a] = 1 - epsilon
-        assert np.isclose(np.sum(self.policy[s, :]), 1)
+        self.P[s, best_a] = 1 - epsilon
+        assert np.isclose(np.sum(self.P[s, :]), 1)
 
     def choose_action(self, s, epsilon):
         self.make_greedy(s, epsilon)
-        return np.random.choice(self.actions['actions'], p=self.policy[s, :])
+        return np.random.choice(self.actions['actions'], p=self.P[s, :])
 
     def choose_sigma(self, mode):
         if mode == 'SARSA':
@@ -122,18 +122,18 @@ class World:
         self.checkpoint = False
 
         if a < (len(self.actions['actions']) / 3):
-            return move_right(s, units, r_border)
+            return self.move_right(s, units, r_border)
         elif a < 2 * (len(self.actions['actions']) / 3):
-            return move_up(s, units, t_border)
+            return self.move_up(s, units, t_border)
         elif a < len(self.actions['actions']):
-            return move_left(s, units, l_border)
+            return self.move_left(s, units, l_border)
         else:
             print('ERROR, incorrcet action!')
             return None
 
     def move_right(self, s, units, r_border):
         for i in units:
-            self.environment['grid'][self.environment['states'][s + i]] = '>'
+            self.environment['grid'][self.states[s + i]] = '>'
             if (s + i) in r_border or (s + i + 1) in self.environment['obstacles']:
                 self.reset()
                 return self.environment['start'], self.rewards['crash']
@@ -141,10 +141,10 @@ class World:
                 self.checkpoint = self.velocity['V_MAX'] != 5
                 self.velocity['V_MAX'] = 5
             elif (s + i + 1) == self.environment['goal']:
-                self.environment['grid'][self.environment['states'][s + i + 1]] = 'O'
+                self.environment['grid'][self.states[s + i + 1]] = 'O'
                 return s + i + 1, self.rewards['win']
 
-        self.environment['grid'][self.environment['states'][s + self.velocity['V']]] = 'O'
+        self.environment['grid'][self.states[s + self.velocity['V']]] = 'O'
         if self.checkpoint:
             return (s + self.velocity['V'], self.rewards['checkpoint']) 
         else:
@@ -153,7 +153,7 @@ class World:
     def move_up(self, s, units, t_border):
         WIDTH = self.environment['size']
         for i in units:
-            self.environment['grid'][self.environment['states'][s - i * WIDTH]] = '|'
+            self.environment['grid'][self.states[s - i * WIDTH]] = '|'
             if (s - i * WIDTH) in t_border or (s - (i + 1) * WIDTH) in self.environment['obstacles']:
                 self.reset()
                 return self.environment['start'], self.rewards['crash']
@@ -161,10 +161,10 @@ class World:
                 self.checkpoint = self.velocity['V_MAX'] != 5
                 self.velocity['V_MAX'] = 5
             elif (s - (i + 1) * WIDTH) == self.environment['goal']:
-                self.environment['grid'][self.environment['states'][s - (i + 1) * WIDTH]] = 'O'
+                self.environment['grid'][self.states[s - (i + 1) * WIDTH]] = 'O'
                 return s - (i + 1) * WIDTH, self.rewards['win']
 
-        self.environment['grid'][self.environment['states'][s + self.velocity['V']]] = 'O'
+        self.environment['grid'][self.states[s + self.velocity['V']]] = 'O'
         if self.checkpoint:
             return (s - self.velocity['V'] * WIDTH, self.rewards['checkpoint']) 
         else:
@@ -172,7 +172,7 @@ class World:
 
     def move_left(self, s, units, l_border):
         for i in units:
-            self.environment['grid'][self.environment['states'][s - i]] = '<'
+            self.environment['grid'][self.states[s - i]] = '<'
             if (s - i) in l_border or (s - i - 1) in self.environment['obstacles']:
                 self.reset()
                 return self.environment['start'], self.rewards['crash']
@@ -180,10 +180,10 @@ class World:
                 self.checkpoint = self.velocity['V_MAX'] != 5
                 self.velocity['V_MAX'] = 5
             elif (s - i - 1) == self.environment['goal']:
-                self.environment['grid'][self.environment['states'][s - i - 1]] = 'O'
+                self.environment['grid'][self.states[s - i - 1]] = 'O'
                 return s - i - 1, self.rewards['win']
 
-        self.environment['grid'][self.environment['states'][s - self.velocity['V']]] = 'O'
+        self.environment['grid'][self.states[s - self.velocity['V']]] = 'O'
         if self.checkpoint:
             return (s - self.velocity['V'], self.rewards['checkpoint']) 
         else:
@@ -206,30 +206,31 @@ def set_args():
 def setup_experiment(world):
     P = np.zeros((len(world.states), len(world.actions['actions'])))
     Q = np.zeros(P.shape)
-    n_step = []
+    n_steps = []
     rewards = []
-    return P, Q, n_step, rewards
+    return P, Q, n_steps, rewards
 
 
 def initialize_episode_args():
-    episode_terms.steps = 0
-    episode_terms.reward = 0
-    episode_terms.states = []
-    episode_terms.actions = []
-    episode_terms.q = []
-    episode_terms.p = []
-    episode_terms.sigmas = [1]
-    episode_terms.targets = []
+    episode_terms = {}
+    episode_terms['steps'] = 0
+    episode_terms['reward'] = 0
+    episode_terms['states'] = []
+    episode_terms['actions'] = []
+    episode_terms['q'] = []
+    episode_terms['p'] = []
+    episode_terms['sigmas'] = [1]
+    episode_terms['targets'] = []
     return episode_terms
 
 
 def initialize_episode(world, episode_terms, args):
-    episode_terms.states.append(world.environment['start'])
+    episode_terms['states'].append(world.environment['start'])
     action = world.choose_action(world.environment['start'], args['epsilon'])
-    episode_terms.actions.append(action)
-    episode_terms.q.append(world.Q[world.environment['start'], action])
-    episode_terms.p.append(world.policy[world.environment['start'], action])
-    return -1, np.inf
+    episode_terms['actions'].append(action)
+    episode_terms['q'].append(world.Q[world.environment['start'], action])
+    episode_terms['p'].append(world.P[world.environment['start'], action])
+    return -1, np.inf, episode_terms
 
 
 def main():
@@ -239,28 +240,61 @@ def main():
     average_reward = []
     
     for _ in range(args['n_experiments']):
-        P, Q, n_step, rewards = setup_experiment(world_instance)
+        P, Q, n_steps, rewards = setup_experiment(world_instance)
         start = dt.now()
 
         for ep in range(args['n_episodes']):
             print('\nEpisode: ' + str(_ + 1) + '/' + str(args['n_episodes']) + " ...")
             world_instance.reset()
             episode_terms = initialize_episode_args()
-            t, T = initialize_episode(world_instance, episode_terms, args)
+            t, T, episode_terms = initialize_episode(world_instance, episode_terms, args)
 
             while True:
                 t += 1
-                assert len(episode_terms.actions) == len(episode_terms.sigmas) == len(episode_terms.p) == len(episode_terms.q)
+                assert len(episode_terms['actions']) == len(episode_terms['sigmas']) == len(episode_terms['p']) == len(episode_terms['q'])
                 if t < T:
-                    s_next, r = world_instance.move(episode_terms.states[t], args['beta'])
-                    episode_terms.states.append(s_next)
-                    episode_terms.steps += 1
-                    episode_terms.reward += r
+                    s_next, r = world_instance.move(episode_terms['states'][t], episode_terms['actions'][t], args['beta'])
+                    episode_terms['states'].append(s_next)
+                    episode_terms['steps'] += 1
+                    episode_terms['reward'] += r
                     if s_next == world_instance.environment['goal']:
                         T = t + 1
-                        episode_terms.targets.append(r - )
-    
-    #  print('HELLO_WORLD')
+                        episode_terms['targets'].append(r - episode_terms['q'][t])
+                    else:
+                        a_next = world_instance.choose_action(episode_terms['states'][t+ 1], args['epsilon'])
+                        episode_terms['actions'].append(s_next)
+                        sigma = world_instance.choose_sigma(args['mode'])
+                        episode_terms['sigmas'].append(sigma)
+                        episode_terms['q'].append(world_instance.Q[s_next, a_next])
+                        target = r + sigma * args['gamma'] * episode_terms['q'][t + 1]
+                        target += (1 - sigma) * args['gamma'] * np.dot(world_instance.P[s_next, :], world_instance.Q[s_next, :])
+                        target += target - episode_terms['q'][t]
+                        episode_terms['targets'].append(target)
+                        episode_terms['p'].append(world_instance.P[s_next, a_next])
+                tau = t - args['n_steps'] + 1
+                if tau >= 0:
+                    E = 1
+                    G = episode_terms['q'][tau]
+                    for k in range(tau, min(t, T - 1)):
+                        G += E * episode_terms['targets'][k]
+                        E *= args['gamma'] * ((1-episode_terms['sigmas']) * episode_terms['p'][k + 1] + episode_terms['sigmas'][k + 1])
+                    error = G - world_instance.Q[episode_terms['states'][tau], episode_terms['actions'][tau]]
+                    world_instance.Q[episode_terms['states'][tau], episode_terms['actions'][tau]] += args['alpha'] * error
+                    world_instance.make_greedy(episode_terms['states'][tau], args['epsilon'])
+                if tau == T - 1:
+                    break
+            
+            print(world_instance.environment['grid'])
+            n_steps.append(episode_terms['steps'])
+            rewards.append(episode_terms['reward'])
+        
+        average_steps.append(np.average(n_steps))
+        print('average number of steps = ' + str(average_steps[-1]))
+
+    print("\nsteps: " + str(average_steps))
+    print("steps avg: " + str(np.average(average_steps)))
+    print("rewards: " + str(average_reward))
+    print("rewards avg: " + str(np.average(average_reward)))
 
 
 if __name__ == "__main__":
